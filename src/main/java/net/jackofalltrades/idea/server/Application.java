0e056cce6d13f1224a11dc8e9f5d46eaea192446
Brad Handy <bhandy@vertafore.com>
2017-12-08 15:17:40 -0500

    Fixed the gradle configuration. Fixed the Freemarker configuration to make the environment properties shared instead of injected by the controller.

--- src/main/java/net/jackofalltrades/idea/server/Application.java
+++ src/main/java/net/jackofalltrades/idea/server/Application.java
@@ -1,7 +1,13 @@
 package net.jackofalltrades.idea.server;
 
+import javax.annotation.PostConstruct;
+
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
